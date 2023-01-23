891fc470ede38fcf6a5d0e3ee934f75d9190b71c
weyrm <76807107+dmswl98@users.noreply.github.com>
2023-01-23 16:02:02 +0900

    design: modal 컴포넌트에 styled-components 적용

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,18 +1,33 @@
-import React from "react";
 import ReactDOM from "react-dom";
+import styled from "styled-components";
 
-import classes from "./Modal.module.css";
+const ModalStyle = styled.div`
+  background-color: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  z-index: 10;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  overscroll-behavior: contain;
+`;
 
 const portalElement = document.getElementById("overlays");
 
 const Modal = (props) => {
   return (
-    <React.Fragment>
+    <>
       {ReactDOM.createPortal(
-        <div className={classes.modal}>{props.children}</div>,
+        <ModalStyle>{props.children}</ModalStyle>,
         portalElement
       )}
-    </React.Fragment>
+    </>
   );
 };
 
